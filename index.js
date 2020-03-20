cc94d5ca6c814357b9d5ad60e1f1c29cbfb52d3a
arturd <arturd@xara.com>
2020-03-20 23:04:02 +0100

    The component itself!

---
+++ index.js
@@ -0,0 +1,37 @@
+import React, { useRef, useState } from 'react'
+import { Animated, UIManager, View, Dimensions, findNodeHandle } from 'react-native'
+const windowHeight = Dimensions.get('window').height;
+
+const DynamicBanner = ({ children, scrollRef, scrollOffset, height = windowHeight / 5 }) => {
+
+	const viewRef = useRef(null);
+	const [yOffset, setYOffset] = useState(0);
+	const [scrollHeight, setScrollHeight] = useState(0);
+	const [childrenHeight, setChildrenHeight] = useState(0);
+
+	return (
+		<View style={{ height, width: '100%', overflow: 'hidden' }} ref={viewRef} onLayout={() => {
+			const scrollHandle = findNodeHandle(scrollRef);
+			viewRef.current.measureLayout(scrollHandle, (_x, y) => setYOffset(y));
+			UIManager.measure(scrollHandle, (_x, _y, _width, height) => setScrollHeight(height));
+		}}>
+			<Animated.View
+				onLayout={event => {
+					setChildrenHeight(event.nativeEvent.layout.height);
+				}}
+				style={{
+					transform: [{
+						translateY: scrollOffset.interpolate({
+							inputRange: [yOffset, yOffset + scrollHeight],
+							outputRange: [0, childrenHeight - height]
+						})
+					}]
+				}}
+			>
+				{children}
+			</Animated.View>
+		</View>
+	)
+};
+
+export default DynamicBanner;
